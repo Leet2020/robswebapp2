60be449c882a527507f325d760423bd5e4bc534e
Rob <rkawak@outlook.com>
2020-09-03 16:11:45 +1000

    initial git commit

---
+++ src/main/java/com/robsapp/robswebapp2/Robswebapp2Application.java
@@ -0,0 +1,13 @@
+package com.robsapp.robswebapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Robswebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Robswebapp2Application.class, args);
+	}
+
+}
